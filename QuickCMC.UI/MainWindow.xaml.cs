9618db0507265ab32ff857adcf1bb982f2474542
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2025-05-18 10:58:28 +0300

    add command

--- QuickCMC.UI/MainWindow.xaml.cs
+++ QuickCMC.UI/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
-﻿using System.Windows;
-using MvvmCross;
-using QuickCMCDemo.MVVMCross.Randomizer;
+﻿using MvvmCross;
 using QuickCMCDemo.MVVMCross.ViewModel;
+using System.Windows;
 
 namespace QuickCMCDemo.UI
 {
@@ -14,31 +13,7 @@ namespace QuickCMCDemo.UI
 		{
 			InitializeComponent();
 
-			startSimulationMenu.IsEnabled = true;
-			stopSimulationMenu.IsEnabled = false;
-
 			DataContext = Mvx.IoCProvider?.Resolve<CompositeViewModel>();
-		}
-
-		private async void StartSimulation_Click(object sender, RoutedEventArgs e)
-		{
-			startSimulationMenu.IsEnabled = false;
-			stopSimulationMenu.IsEnabled = true;
-
-			await Mvx.IoCProvider?.Resolve<IAnalogOutputsRandomizer>()?.StartSimulation();
-		}
-
-		private async void StopSimulation_Click(object sender, RoutedEventArgs e)
-		{
-			startSimulationMenu.IsEnabled = true;
-			stopSimulationMenu.IsEnabled = false;
-
-			await Mvx.IoCProvider?.Resolve<IAnalogOutputsRandomizer>()?.StopSimulation();
-		}
-
-		private void Exit_Click(object sender, RoutedEventArgs e)
-		{
-			Application.Current.Shutdown();
 		}
 	}
 }
--- QuickCMCDemo.MVVMCross/ViewModel/CompositeViewModel.cs
+++ QuickCMCDemo.MVVMCross/ViewModel/CompositeViewModel.cs
@@ -1,26 +1,55 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross;
+using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using QuickCMCDemo.MVVMCross.Randomizer;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace QuickCMCDemo.MVVMCross.ViewModel
 {
-    public sealed class CompositeViewModel : MvxViewModel
-    {
-        #region Fields
-        private AnalogOutputsViewModel? _analogOutputsVM;
-        #endregion
+	public sealed class CompositeViewModel : MvxViewModel
+	{
+		#region Fields
+		private AnalogOutputsViewModel? _analogOutputsVM;
+		#endregion
 
-        #region Ctor
-        public CompositeViewModel(AnalogOutputsViewModel analogOutputsVM)
-        {
-            AnalogOutputsVM = analogOutputsVM;
-        }
-        #endregion
+		#region Ctor
+		public CompositeViewModel(AnalogOutputsViewModel analogOutputsVM)
+		{
+			AnalogOutputsVM = analogOutputsVM;
 
-        #region Dependency Properties
-        public AnalogOutputsViewModel? AnalogOutputsVM
-        {
-            get => _analogOutputsVM;
-            set => SetProperty(ref _analogOutputsVM, value);
-        }
-        #endregion
-    }
+			ExitCommand = new MvxCommand(Exit);
+			StartSimulationCommand = new MvxCommand(async () => await StartSimulation());
+			StopSimulationCommand = new MvxCommand(async () => await StopSimulation());
+		}
+		#endregion
+
+		#region Dependency Properties
+		public AnalogOutputsViewModel? AnalogOutputsVM
+		{
+			get => _analogOutputsVM;
+			set => SetProperty(ref _analogOutputsVM, value);
+		}
+		public IMvxCommand ExitCommand { get; }
+		public IMvxCommand StartSimulationCommand { get; }
+		public IMvxCommand StopSimulationCommand { get; }
+		#endregion
+
+		#region Methods
+		private void Exit()
+		{
+			Application.Current.Shutdown();
+		}
+
+		private async Task StartSimulation()
+		{
+			await Mvx.IoCProvider?.Resolve<IAnalogOutputsRandomizer>()?.StartSimulation();
+		}
+
+		private async Task StopSimulation()
+		{
+			await Mvx.IoCProvider?.Resolve<IAnalogOutputsRandomizer>()?.StopSimulation();
+		}
+		#endregion
+	}
 }
