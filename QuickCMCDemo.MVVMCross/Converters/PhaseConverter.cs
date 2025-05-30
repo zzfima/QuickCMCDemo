a47fb785ad71fd11b9dc468c672d7b453902ca07
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2025-05-30 11:08:53 +0300

    RFCT

--- QuickCMCDemo.MVVMCross/Converters/PhaseConverter.cs
+++ QuickCMCDemo.MVVMCross/Converters/PhaseConverter.cs
@@ -4,22 +4,22 @@ using System.Windows.Data;
 
 namespace QuickCMCDemo.MVVMCross.Converters
 {
-    public class PhaseConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is double d)
-                return d.ToString("F2", culture) + " *";
+	public class PhaseConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is double d)
+				return d.ToString("F2", culture) + " *";
 
-            return value?.ToString() ?? string.Empty;
-        }
+			return value?.ToString() ?? string.Empty;
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (targetType == typeof(double) && double.TryParse(value?.ToString(), out double d))
-                return d;
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType == typeof(double) && double.TryParse(value?.ToString(), out double d))
+				return d;
 
-            return Binding.DoNothing;
-        }
-    }
+			return Binding.DoNothing;
+		}
+	}
 }
